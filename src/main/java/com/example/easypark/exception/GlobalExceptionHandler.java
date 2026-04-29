5813c560390b46392274678820ca089f13d99d20
Luiz Henrique Gomes <luiz.hgsilva@hotmail.com>
2026-04-28 22:20:50 -0300

    feat: add handler and API error

--- src/main/java/com/example/easypark/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/easypark/exception/GlobalExceptionHandler.java
@@ -1,14 +1,39 @@
 package com.example.easypark.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    // Regra de negócio
     @ExceptionHandler(BusinessException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
-    public String handleBusinessException(BusinessException ex) {
-        return ex.getMessage();
+    public ApiError handleBusinessException(
+            BusinessException ex,
+            HttpServletRequest request
+    ) {
+        return new ApiError(
+                409,
+                "Business Error",
+                ex.getMessage(),
+                request.getRequestURI()
+        );
+    }
+
+    // Erro inesperado
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ApiError handleGenericException(
+            Exception ex,
+            HttpServletRequest request
+    ) {
+        return new ApiError(
+                500,
+                "Internal Server Error",
+                ex.getMessage(),
+                request.getRequestURI()
+        );
     }
 }
